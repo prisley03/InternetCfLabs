20e2bbe0b6d6b27a6d95e52ba8ca8c416b10a336
prisley03 <prillysci.laindra@gmail.com>
2023-12-17 02:59:46 +0700

    yeay done

--- projectOOAD/src/controller/JobController.java
+++ projectOOAD/src/controller/JobController.java
@@ -1,5 +1,37 @@
 package controller;
 
-public class JobController {
+import java.util.ArrayList;
 
+import model.database.JobDatabase;
+import model.object.Job;
+import model.object.PC;
+
+public class JobController {
+	private static class SingletonHelper{
+		private static final JobController INSTANCE = new JobController();
+		private static final JobDatabase jobDB = new JobDatabase();
+	}
+	
+	public static JobController getInstance() {
+		return SingletonHelper.INSTANCE;
+	}
+	
+	public ArrayList<Job> getAllJobData(){
+		return SingletonHelper.jobDB.getAllData();
+	}
+	
+	public ArrayList<Job> getJobUncompleteData(){
+		return SingletonHelper.jobDB.getJobUncompleteData();
+	}
+	
+	public void markComplete(int pcId) {
+		ArrayList<Job>jobUncompleteData = SingletonHelper.jobDB.getJobUncompleteData();
+		
+		for(Job job: jobUncompleteData) {
+			if(job.getPcId() == pcId) {
+				SingletonHelper.jobDB.update(job);
+			}
+		}
+	}
+	
 }
